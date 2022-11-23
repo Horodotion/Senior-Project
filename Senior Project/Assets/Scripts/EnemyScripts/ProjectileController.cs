76a49ee33737d34cb65ab5beb78f2fa4f1233071
Horodotion <85849410+Horodotion@users.noreply.github.com>
2022-11-22 18:41:25 -0800

    Spell fixes, projectie rework, and spawnmanager

--- Senior Project/Assets/Scripts/EnemyScripts/ProjectileController.cs
+++ Senior Project/Assets/Scripts/EnemyScripts/ProjectileController.cs
@@ -4,68 +4,86 @@ using UnityEngine;
 
 public class ProjectileController : MonoBehaviour
 {
-    public ProjectileManager ourProjectileManager;
-
-    public Rigidbody rb;
+    [HideInInspector] public Rigidbody rb;
     public Faction hostileFaction;
     public float projectileSpeed;
     public float lifeSpan;
+    public float range;
+    [HideInInspector] public Vector3 origin;
     public float damage;
 
     public GameObject destroyEffectPrefab;
     // On fresh prefabs I set health to 10 by default, but feel free to change if we have a global damage scale
-    public float explosionRadius, baseDamageDealt, secondsUntilParticlesAreDestroyed;
+    // public float explosionRadius, baseDamageDealt, secondsUntilParticlesAreDestroyed;
 
-    void Start()
+    public virtual void Awake()
     {
         if (GetComponent<Rigidbody>() != null)
         {
             rb = GetComponent<Rigidbody>();
-            rb.AddForce(transform.forward * projectileSpeed, ForceMode.VelocityChange);
         }
-        
-        Destroy(gameObject, lifeSpan);
+        else
+        {
+            Debug.Log(gameObject.name + " needs a rigidbody");
+            gameObject.SetActive(false);
+        }
     }
 
-    void OnTriggerEnter(Collider col)
+    public virtual void FixedUpdate()
+    {
+        if (Vector3.Distance(transform.position, origin) >= range)
+        {
+            Deactivate();
+        }
+    }
+
+    public virtual void OnTriggerEnter(Collider col)
     {
         if (col.gameObject.tag == "Enemy" && col.gameObject.GetComponent<EnemyController>() != null)
         {
             col.gameObject.GetComponent<EnemyController>().Damage(damage);
         }
-        // else if (col.gameObject.tag != "Projectile")
-        // {
-        //     Destroy(gameObject);
-        // }
     }
 
-    public void Explode()
+    // public virtual void Explode()
+    // {
+    //     GameObject destructionParticles = Instantiate(destroyEffectPrefab, transform.position, Quaternion.Euler(0, 0, 0));
+    //     Destroy(destructionParticles, secondsUntilParticlesAreDestroyed);
+
+    //     Collider[] hitColliders = Physics.OverlapSphere(transform.position, explosionRadius);
+    //     foreach (var hC in hitColliders)
+    //     {
+    //         var damage = baseDamageDealt - Vector3.Distance(transform.position, hC.transform.position);
+    //         damage = Mathf.Clamp(baseDamageDealt, 0, damage);
+
+    //         // INSERT HERE: Function or however damage is assigned, pass each object returned in hitColliders the damage variable above
+    //         if (hC.gameObject.tag == "Player" && hC.GetComponent<PlayerPuppet>() != null)
+    //         {
+    //             hC.GetComponent<PlayerPuppet>().Damage(damage);
+    //         }
+    //         if (hC.gameObject.tag == "Enemy" && hC.GetComponent<EnemyController>() != null && !hC.GetComponent<EnemyController>().dead)
+    //         {
+    //             hC.GetComponent<EnemyController>().Damage(damage);
+    //         }
+
+    //         // Optional addition: Explosion force equal to damage, originating from this object's position. Remove if not wanted
+    //         if (hC.GetComponent<Rigidbody>() != null)
+    //         {
+    //             hC.GetComponent<Rigidbody>().AddExplosionForce(damage * 5, transform.position, explosionRadius, 1f);
+    //         }
+    //     }
+    //     Destroy(gameObject);
+    // }
+
+    public virtual void LaunchProjectile()
     {
-        GameObject destructionParticles = Instantiate(destroyEffectPrefab, transform.position, Quaternion.Euler(0, 0, 0));
-        Destroy(destructionParticles, secondsUntilParticlesAreDestroyed);
+        origin = gameObject.transform.position;
+        rb.AddForce(transform.forward * projectileSpeed, ForceMode.VelocityChange);
+    }
 
-        Collider[] hitColliders = Physics.OverlapSphere(transform.position, explosionRadius);
-        foreach (var hC in hitColliders)
-        {
-            var damage = baseDamageDealt - Vector3.Distance(transform.position, hC.transform.position);
-            damage = Mathf.Clamp(baseDamageDealt, 0, damage);
-
-            // INSERT HERE: Function or however damage is assigned, pass each object returned in hitColliders the damage variable above
-            if (hC.gameObject.tag == "Player" && hC.GetComponent<PlayerPuppet>() != null)
-            {
-                hC.GetComponent<PlayerPuppet>().Damage(damage);
-            }
-            if (hC.gameObject.tag == "Enemy" && hC.GetComponent<EnemyController>() != null && !hC.GetComponent<EnemyController>().dead)
-            {
-                hC.GetComponent<EnemyController>().Damage(damage);
-            }
-
-            // Optional addition: Explosion force equal to damage, originating from this object's position. Remove if not wanted
-            if (hC.GetComponent<Rigidbody>() != null)
-            {
-                hC.GetComponent<Rigidbody>().AddExplosionForce(damage * 5, transform.position, explosionRadius, 1f);
-            }
-        }
-        Destroy(gameObject);
+    public virtual void Deactivate()
+    {
+        rb.velocity = Vector3.zero;
+        gameObject.SetActive(false);
     }
 }
