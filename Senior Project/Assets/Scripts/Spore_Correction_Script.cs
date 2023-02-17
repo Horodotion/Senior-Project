471973d7d3a6daa82e464c9e9c025e2daa3701f5
Clownf1sh <67593967+Clownf1sh@users.noreply.github.com>
2023-02-16 21:05:10 -0800

    Spores

---
+++ Senior Project/Assets/Scripts/Spore_Correction_Script.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spore_Correction_Script : MonoBehaviour
+{
+    public GameObject sporeEffect;
+
+    private void Awake()
+    {
+        sporeEffect.transform.Rotate ((-this.transform.rotation.x*100),0,0);
+        sporeEffect.transform.lossyScale.Set(this.transform.localScale.x, this.transform.localScale.y, this.transform.localScale.z);
+    }
+}
