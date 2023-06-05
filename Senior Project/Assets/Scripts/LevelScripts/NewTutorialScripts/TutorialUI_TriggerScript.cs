8d7db5cb411de60d3b67db908b438cd79ca2b48a
Felix Nicolasin <jusnic1@yahoo.com>
2023-06-05 15:23:35 -0700

    Tutorial UI Fix

--- Senior Project/Assets/Scripts/LevelScripts/NewTutorialScripts/TutorialUI_TriggerScript.cs
+++ Senior Project/Assets/Scripts/LevelScripts/NewTutorialScripts/TutorialUI_TriggerScript.cs
@@ -7,6 +7,7 @@ public class TutorialUI_TriggerScript : TriggerScript
     [Header("References")]
     public TutorialUIManager manager;
     [SerializeField] private GameObject[] tutorialElements;
+    private bool isIn = false;
 
     [Header("Settings")]
     [SerializeField] private int elementToCall;
@@ -17,13 +18,23 @@ public class TutorialUI_TriggerScript : TriggerScript
         tutorialElements = manager.UI_Elements;
     }
 
+    private void Update()
+    {
+        if (PlayerController.ourPlayerState == PlayerState.inMenu && isIn ==true)
+        {
+            tutorialElements[elementToCall].SetActive(false);
+        }
+    }
+
     public override void ActionToTrigger()
     {
         tutorialElements[elementToCall].SetActive(true);
+        isIn = true;
     }
 
     public override void ActionToStop()
     {
         tutorialElements[elementToCall].SetActive(false);
+        isIn = false;
     }
 }
