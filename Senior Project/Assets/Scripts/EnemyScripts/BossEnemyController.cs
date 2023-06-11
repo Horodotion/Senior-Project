2b35dd03f20b47a24251cb99e21bba72afd2f781
leokwok2021 <85981052+leokwok2021@users.noreply.github.com>
2023-06-11 09:22:59 -0700

    6_11_2023_Boss_Laser_Tinkering

--- Senior Project/Assets/Scripts/EnemyScripts/BossEnemyController.cs
+++ Senior Project/Assets/Scripts/EnemyScripts/BossEnemyController.cs
@@ -231,7 +231,7 @@ public class BossEnemyController : EnemyController
 
         bossState = BossState.idle;
 
-        bossState = BossState.laserAttack;
+        bossState = BossState.meleeAttack;
         //bossState = BossState.spawnTurrets;
         //animator.SetBool(aniDeathDecision, true);
         //bossState = BossState.laserAttack;
@@ -1072,6 +1072,7 @@ public class BossEnemyController : EnemyController
         }
         return false;
     }
+    //Aim Horizontaly
     public virtual void AimTowards(Vector3 position, float aimSpeed)
     {
         Vector3 veiwToPlayerMesh = position - viewPoint.transform.position;
@@ -1079,6 +1080,13 @@ public class BossEnemyController : EnemyController
         transform.forward = Vector3.RotateTowards(transform.forward, veiwToPlayerMesh, aimSpeed * Time.deltaTime, 0.0f);
         Debug.DrawRay(viewPoint.transform.position, veiwToPlayerMesh, Color.blue);
     }
+    public virtual void AimTowards(GameObject gameObject, Vector3 position, float aimSpeed)
+    {
+        Vector3 veiwToPlayerMesh = position - viewPoint.transform.position;
+        veiwToPlayerMesh.y = 0;
+        gameObject.transform.forward = Vector3.RotateTowards(gameObject.transform.forward, veiwToPlayerMesh, aimSpeed * Time.deltaTime, 0.0f);
+    }
+
 
     public virtual void AimTowardsWithY(GameObject gameObject, Vector3 position, float aimSpeed)
     {
